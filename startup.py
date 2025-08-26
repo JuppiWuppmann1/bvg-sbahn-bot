4e4ee33f1dbe7168ccf7d1f57f7c8ab4016879e8
JuppiWuppmann1 <maxlipsius@gmail.com>
2025-08-26 19:43:09 +0200

    Update startup.py

--- startup.py
+++ startup.py
@@ -1,5 +1,4 @@
-import asyncio
 from playwright.__main__ import main as playwright_main
 
-async def install_playwright():
-    await asyncio.to_thread(playwright_main, ["install", "chromium"])
+def install_playwright():
+    playwright_main(["install", "chromium"])

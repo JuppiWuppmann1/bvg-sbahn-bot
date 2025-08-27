e505eb00fc4fd89ac8c2cd49f2738da363cd632d
JuppiWuppmann1 <maxlipsius@gmail.com>
2025-08-27 19:11:56 +0200

    Update startup.py

--- startup.py
+++ startup.py
@@ -3,7 +3,7 @@ import logging
 
 def install_playwright():
     try:
-        subprocess.run(["playwright", "install", "chromium"], check=True)
+        subprocess.run(["python", "-m", "playwright", "install", "chromium"], check=True)
         logging.info("✅ Playwright-Browser erfolgreich installiert.")
     except subprocess.CalledProcessError as e:
         logging.error(f"❌ Fehler bei der Playwright-Installation: {e}")

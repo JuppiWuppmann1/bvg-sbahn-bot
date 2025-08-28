c190ef29054bdc2b4c9162a7117f92244eb81c5d
JuppiWuppmann1 <maxlipsius@gmail.com>
2025-08-28 18:38:26 +0200

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -4,5 +4,11 @@ set -o errexit
 echo "🚀 Installiere Python-Pakete..."
 pip install -r requirements.txt
 
+echo "📦 Installiere systemweiten Chrome..."
+apt-get update && apt-get install -y chromium-browser
+
 echo "🔧 Installiere Node-Abhängigkeiten..."
 npm install
+
+echo "🚀 Installiere Playwright-Browser..."
+python -m playwright install chromium

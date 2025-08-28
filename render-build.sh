5583bb353fe543f63bbe5fe2238ca6d67b3ecb1f
JuppiWuppmann1 <maxlipsius@gmail.com>
2025-08-28 19:39:33 +0200

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -4,5 +4,8 @@ set -o errexit
 echo "🚀 Installiere Python-Pakete..."
 pip install -r requirements.txt
 
+echo "📦 Installiere systemweiten Chromium..."
+apt-get update && apt-get install -y chromium
+
 echo "🔧 Installiere Node-Abhängigkeiten..."
 npm install

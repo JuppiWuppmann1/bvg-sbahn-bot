1f37e6247bd9489eed0eb8b51057e0ef43ea3269
JuppiWuppmann1 <maxlipsius@gmail.com>
2025-08-30 20:09:22 +0200

    Delete thread_builder.py

--- thread_builder.py
+++
@@ -1,9 +0,0 @@
-def build_threads(meldungen):
-    threads = []
-    for meldung in meldungen:
-        parts = [
-            f"**{meldung['quelle']}**\n{meldung['titel']}",
-            meldung["beschreibung"]
-        ]
-        threads.append(parts)
-    return threads
